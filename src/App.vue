52b656a678020bd715535e2dca4dac1483bac71d
Ayomidespenz <122951762+Ayomidespenz@users.noreply.github.com>
2025-05-27 09:45:30 +0100

    fixedd

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <!-- <Navbar/> -->
+    <Navbar/>
     <router-view />
   </div>
 </template>
 
 <script>
-
+import Navbar from "./components/Navbar.vue"
 
 export default {
   name: 'App',
 
-  //  components: {  Navbar }
+   components: {  Navbar }
 
 }
 </script>
